df7d2b7fcfee5d0a91f15c269454ab753d15a8fd
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-01-21 13:37:03 -0500

    comentarios

--- logic/movies.py
+++ logic/movies.py
@@ -1,10 +1,10 @@
 import json
 
-def seeMovies():
+def seeMovies(): #Ver las peliculas del json
     with open("data/movies.json", "r", encoding="utf-8") as file:
             return json.load(file)
     
-def updateJsonWhenSaveMovies():
+def updateJsonWhenSaveMovies(): #Actualizar al haber un cambio
     from design.movies import temporalMovies
     list = temporalMovies
     with open("data/movies.json", "w", encoding="utf-8") as file:
@@ -14,71 +14,71 @@ def updateJsonWhenSaveMovies():
     temporalMovies.clear()
 
 
-def editMoviesTitle(movie_id, new_title): 
+def editMoviesTitle(movie_id, new_title):  #Editar titulo peliculas
     from design.movies import temporalMovies
     for movie in temporalMovies:
-        if movie["ID"] == movie_id.strip():
-            movie["Titulo"] = new_title  
+        if movie["ID"] == movie_id.strip(): #si el id existe
+            movie["Titulo"] = new_title  #actualiza el titulo
             print("Se ha actualizado el título en la lista temporal")
             break
 
-def editMovieDirection(movie_id, direction): 
-    from design.movies import temporalMovies
-    for movie in temporalMovies:
-        if movie["ID"] == movie_id.strip():
-            movie["Direccion"] = direction  
+def editMovieDirection(movie_id, direction): #Editar el director
+    from design.movies import temporalMovies 
+    for movie in temporalMovies: 
+        if movie["ID"] == movie_id.strip(): #si el id existe
+            movie["Direccion"] = direction  #actualiza el director
 
             print("Se ha actualizado el director en la lista temporal")
             break
 
-def editMovieGenre(movie_id): 
+def editMovieGenre(movie_id): #Editar genero de la pelicula
     from design.movies import temporalMovies
 
     for movie in temporalMovies:
-        if movie["ID"] == movie_id.strip():
-            new_genre = []
-            while True:
+        if movie["ID"] == movie_id.strip(): #si el id existe
+            new_genre = [] 
+            while True: #Ciclo para agregar
                 opcion = input("Ingrese el genero nuevo: ")
                 new_genre.append(opcion)
                 opc = input("Quiere agregar otro genero? (s/n): ").strip()
                 if opc.lower() != "s":
                     break
                 if new_genre:
-                    movie["Genero"] = new_genre
+                    movie["Genero"] = new_genre #actualiza el genero
                 else:
                     print("No se han ingresado generos")
 
-def editMovieProduction(movie_id, production): 
+def editMovieProduction(movie_id, production): #Editar el productor de la pelicula
     from design.movies import temporalMovies
     for movie in temporalMovies:
-        if movie["ID"] == movie_id.strip():
-            movie["Producción"] = production  
+        if movie["ID"] == movie_id.strip(): #si el id existe
+            movie["Producción"] = production #Actualiza el productor
 
             print("Se ha actualizado la producción en la lista temporal")
             break
 
-def editMovieRate(movie_id, rate): 
-    from design.movies import temporalMovies
+def editMovieRate(movie_id, rate): #Editar la valoracion de la pelicula
+    from design.movies import temporalMovies 
     for movie in temporalMovies:
-        if movie["ID"] == movie_id.strip():
-            movie["Valoracion"] = rate  
+        if movie["ID"] == movie_id.strip(): #si el id existe
+            movie["Valoracion"] = rate   #Actualiza la valoracion
             print("Se ha actualizado la valoracion en la lista temporal")
             break
 
-def editMovieCAT(movie_id, CAT): 
+def editMovieCAT(movie_id, CAT):  #Editar la categoria de la pelicula
     from design.movies import temporalMovies
     for movie in temporalMovies:
-        if movie["ID"] == movie_id.strip(): 
-            movie["Categoria"] = CAT  
+        if movie["ID"] == movie_id.strip(): #si el id existe
+            movie["Categoria"] = CAT  #actualiza la categoria
             print("Se ha actualizado la categoria en la lista temporal")
             break
 
-def deleteTEMPORALMovie(id): 
+def deleteTEMPORALMovie(id): #borrar de la lista temporal
     from design.movies import temporalMovies
     info = temporalMovies
     
     for code in info: 
-        if id.strip() == code.get("ID"): 
+        if id.strip() == code.get("ID"): #si el id existe
             security = input("¿Está seguro de eliminar la pelicula? (s/n): ".strip())
             if security.lower() == "s":  
                 info.remove(code)  
@@ -89,12 +89,12 @@ def deleteTEMPORALMovie(id):
                 break
     input("Presione enter para continuar -->")     
 
-def deleteTEMPORALMoviebyName(title): 
+def deleteTEMPORALMoviebyName(title): #borrar de la lista temporal
     from design.movies import temporalMovies
     info = temporalMovies
     
     for code in info: 
-        if title == code.get("Titulo"): 
+        if title == code.get("Titulo"):  #borra por nombre
             security = input("¿Está seguro de eliminar la pelicula? (s/n): ".strip())
             if security.lower() == "s":  
                 info.remove(code)  
--- logic/music.py
+++ logic/music.py
@@ -1,9 +1,9 @@
 import json
 
-def seeMusic():
+def seeMusic(): #Ver musica del json
     with open("data/music.json", "r", encoding="utf-8") as file:
             return json.load(file)
-def updateJsonWhenSaveMusic():
+def updateJsonWhenSaveMusic(): #Guardar cambios en el json
     from design.music import temporalSongs
     list = temporalSongs
     with open("data/music.json", "w", encoding="utf-8") as file:
@@ -15,24 +15,24 @@ def updateJsonWhenSaveMusic():
 def editMusicTitle(music_id, new_title): #Editar titulo que recibe id y nuevo nombre
     from design.music import temporalSongs
     for song in temporalSongs:
-        if song["ID"] == music_id.strip():
-            song["Titulo"] = new_title  
+        if song["ID"] == music_id.strip(): #si el id existe
+            song["Titulo"] = new_title   #actualiza el titulo
             print("Se ha actualizado el título en la lista temporal")
             break
 
 def editMusicAutor(music_id, new_autor): #Editar autor que recibe id y nuevo nombre
     from design.music import temporalSongs
     for song in temporalSongs:
-        if song["ID"] == music_id.strip():
-            song["Autor"] = new_autor 
+        if song["ID"] == music_id.strip(): #si el id existe
+            song["Autor"] = new_autor  #actualiza el autor
             print("Se ha actualizado el autor en la lista temporal")
             break
 
 def editMusicAlbum(music_id, album): #Editar album que recibe id y el nuevo nombre
     from design.music import temporalSongs
     for song in temporalSongs:
-        if song["ID"] == music_id.strip():
-            song["Album"] = album 
+        if song["ID"] == music_id.strip(): #si el id existe
+            song["Album"] = album  #actualiza el album
             print("Se ha actualizado el album en la lista temporal")
             break
 
@@ -40,7 +40,7 @@ def editMusicGenre(music_id):
     from design.music import temporalSongs
 
     for music in temporalSongs:
-        if music["ID"] == music_id.strip():
+        if music["ID"] == music_id.strip(): #si el id existe
             new_genre = []
             while True:
                 opcion = input("Ingrese el genero nuevo: ")
@@ -49,23 +49,23 @@ def editMusicGenre(music_id):
                 if opc.lower() != "s":
                     break
                 if new_genre:
-                    music["Genero"] = new_genre
+                    music["Genero"] = new_genre #actualiza el genero
                 else:
                     print("No se han ingresado generos")
 
 def editMusicCAT(music_id, cat): #Editar categoria que recibe id y nueva categoria
     from design.music import temporalSongs
     for song in temporalSongs:
-        if song["ID"] == music_id.strip():
-            song["Categoria"] = cat 
+        if song["ID"] == music_id.strip():#si el id existe
+            song["Categoria"] = cat #actualiza la categoria
             print("Se ha actualizado la categoria en la lista temporal")
             break
 
 def editMusicDisc(music_id, disc): #Editar discografia que recive id y el nuevo nombre
     from design.music import temporalSongs
     for song in temporalSongs:
-        if song["ID"] == music_id.strip():
-            song["Discografica"] = disc
+        if song["ID"] == music_id.strip(): #si el id existe
+            song["Discografica"] = disc #actualiza la discografica
             print("Se ha actualizado la discografica en la lista temporal")
             break
 
@@ -74,7 +74,7 @@ def deleteTEMPORALSong(id): #Borrar musica por id
     info = temporalSongs
     
     for code in info: 
-        if id == code.get("ID"): 
+        if id == code.get("ID"):  #si el id existe
             security = input("¿Está seguro de eliminar la cancion? (s/n): ".strip())
             if security.lower() == "s":  
                 info.remove(code)  
@@ -90,7 +90,7 @@ def deleteTEMPORALSongbyName(title): #Borrar musica por el nombre
     info = temporalSongs
     
     for code in info: 
-        if title == code.get("Titulo"): 
+        if title == code.get("Titulo"): #Si el titulo existe
             security = input("¿Está seguro de eliminar la cancion? (s/n): ".strip())
             if security.lower() == "s":  
                 info.remove(code)  
